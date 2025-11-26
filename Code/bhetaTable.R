e52bfe50e02e63e09fdb8cdf261d30cea676cc83
joRauser <jonas.rauser@web.de>
2025-11-26 17:19:01 +0100

    Update bhetaTable.R

--- Code/bhetaTable.R
+++ Code/bhetaTable.R
@@ -1,22 +1,19 @@
 library(quantreg)
 
-#### Für ein Quantil: 
+####### exemplary for a single quantile: ####### 
 
-# Quantilsregression schätzen (z.B. Median-Regression)
+# estimate quantreg
 testMod <- rq(viewCount ~ 1, tau = 0.75, data = vidStatsMC)
 
-# Zusammenfassung des Modells (ähnlich wie summary(lm()))
+# summarise the model
 test_summaryModel <- summary(testMod, se = "nid")  # "nid" = robust standard errors
 
-# Betas + Signifikanzen extrahieren
+# extract betas and significances
 test_coefs <- test_summaryModel$coefficients
 
 
-# Ausgabe anzeigen
-print(test_coefs)
 
-
-#### Function für mehrere Quantile:
+####### function for multiple quantiles #######
 library(quantreg)
 library(dplyr)
 library(tidyr)
@@ -24,7 +21,7 @@ library(purrr)
 
 quantRegCoef <- function(formula, taus, data) {
   
-  # Funktion zur Schätzung eines Modells + Extraktion der Betas
+  # estimate model and extract bhetas
   get_betas <- function(tau) {
     mod <- rq(formula, tau = tau, data = data)
     sm  <- summary(mod, se = "nid")
@@ -45,11 +42,11 @@ quantRegCoef <- function(formula, taus, data) {
     return(betaStars)
   }
   
-  # Für jedes Tau Koeffizienten extrahieren
+  # extract coefficient for every tau
   beta_list <- map(taus, get_betas)
   names(beta_list) <- taus
   
-  # In breites Format bringen
+  # format
   beta_df <- beta_list %>% 
     bind_cols() %>%
     mutate(Variable = rownames(summary(rq(formula, tau = taus[1], data = data))$coefficients)) %>%
@@ -58,17 +55,9 @@ quantRegCoef <- function(formula, taus, data) {
   return(beta_df)
 }
 
+# execute the function
 bhetaFullMod <- quantRegCoef(
   formula = viewCount ~ artistFollower + durationMinutes + concertType + caption + concertNumber,
   taus = c(0.1, 0.25, 0.5, 0.75, 0.9),
   data = vidStatsMC
 )
-
-bhetaConstMod <- quantRegCoef(
-  formula = viewCount ~ 1,
-  taus = c(0.1, 0.25, 0.5, 0.75, 0.9),
-  data = vidStatsMC
-)
-
-# Bheta-Tabelle in Latex übertragen. Dabei aber auch mit den P-Values eintragen (jeweils einfach als Sternchen unter die Werte)
-# Nur mit den Sternchen sind Aussagen über die Werte auch von Relevanz
